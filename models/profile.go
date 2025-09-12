df60376aa48cc3530353726a34b1fa8904eb9d6b
zyj <18107291228@163.com>
2025-09-12 18:30:11 +0800

    修改数据库模型

--- models/profile.go
+++ models/profile.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Profile struct {
-	ID          int8      `gorm:"id;primary_key"`
-	UserId      int8      `gorm:"user_id"`
+	ID          int64     `gorm:"id;primary_key"`
+	UserId      int64     `gorm:"user_id"`
 	Nickname    string    `gorm:"nickname"`
 	Email       string    `gorm:"email"`
 	Phone       string    `gorm:"phone"`
